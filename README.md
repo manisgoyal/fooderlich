6503b0c2249edddd8e34fad4a21d217125718896
Manish Goyal <manishgoyal016@gmail.com>
2022-03-03 23:34:37 +0545

    Upto Theming in Chapter 3

---
+++ README.md
@@ -0,0 +1,3 @@
+# fooderlich
+
+A flutter project for section 2 chapter 3 raywenderlich's book 
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+
+void main() => runApp(const FooderLich());
+
+class FooderLich extends StatelessWidget {
+  const FooderLich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    // TODO: Apply Home widget
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: Scaffold(
+        appBar:
+            AppBar(title: Text('Fooderlich', style: theme.textTheme.headline6)),
+        body: Center(
+          child: Text(
+            'Let\'s get cooking 👩‍🍳',
+            style: theme.textTheme.headline1,
+          ),
+        ),
+      ),
+    );
+  }
+}
